c5acade496b2ef96138fad4eff53e1323489ae68
VictorKaue <victorkaue909@gmail.com>
2024-08-07 08:58:45 -0300

    commit de opeacoes de matrizes

--- Operações de matrizes.c
+++ Operações de matrizes.c
@@ -8,7 +8,8 @@ typedef struct{
     int colunas;
 }Matrizes;
 
-Matrizes *alocar_matriz(int operacao, int quanti_matrizes);
+Matrizes *alocar_matriz(int operacao, int tamanho);
+Matrizes *alocar_matriz_multiplicacao(int *quanti_matrizes);
 void desalocar_matriz(Matrizes **matrizes, int desalocar, int quanti_matrizes);
 void atribuir_valores(Matrizes *matrizes, int quanti_matrizes); 
 void imprimir_matrizes(Matrizes *matrizes, int quanti_matrizes);
@@ -19,13 +20,17 @@ void multiplicacao(Matrizes *matrizes, int quanti_matrizes);
 
 int main(){
     int operacao,quanti_matriz;
-
+    Matrizes *matrizes;
     printf("digite qual operação de matrizes deseja realizar\n[1] Soma\n[2] Subtração\n[3] Multiplicação\n[4] Divisão\n[5] transpor a matriz\n");
     scanf("%d", &operacao);
     printf("Digite com quantas matrizes deseja fazer a operação: ");
     scanf("%d",&quanti_matriz);
     quanti_matriz++;
-    Matrizes *matrizes = alocar_matriz(operacao, quanti_matriz);
+    if(operacao == 3){
+        matrizes = alocar_matriz_multiplicacao(&quanti_matriz);
+    } else {
+        matrizes = alocar_matriz(operacao, quanti_matriz);
+    }
     atribuir_valores(matrizes, quanti_matriz);
     if(matrizes==NULL){
         printf("Erro ao alocar matrizes\n");
@@ -58,23 +63,27 @@ Matrizes *alocar_matriz(int operacao, int tamanho){
         printf("Erro ao alocar memória para a struct!\n");
         return NULL;
     }
-    
-    for(i=0; i<tamanho; i++){
-        if(operacao == 1 || operacao == 2){
-            int linha, coluna;
-            if(i==0){
-                printf("Digite quantas quantas linhas e quantas colunas para cada matriz: ");
-                scanf("%d %d", &linha, &coluna);
-            }
-            matrizes[i].linhas = linha;
-            matrizes[i].colunas = coluna;
 
-        } else {
-            printf("Digite a quantidade de linhas para a %dª matriz: ", i+1);
-            scanf("%d", &matrizes[i].linhas);
-            printf("digite a quantidade de colunas para a %dª matriz: ", i+1);
-            scanf("%d", &matrizes[i].colunas);
+    for(i=0; i<tamanho; i++){
+    switch (operacao)
+    {
+    case 1:
+    case 2:
+        int linha, coluna;
+        if(i==0){
+            printf("Digite quantas quantas linhas e quantas colunas para cada matriz: ");
+            scanf("%d %d", &linha, &coluna);
         }
+        matrizes[i].linhas = linha;
+        matrizes[i].colunas = coluna;
+        break;
+    default:
+        printf("Digite a quantidade de linhas para a %dª matriz: ", i+1);
+        scanf("%d", &matrizes[i].linhas);
+        printf("digite a quantidade de colunas para a %dª matriz: ", i+1);
+        scanf("%d", &matrizes[i].colunas);
+        break;
+    }
         matrizes[i].matriz = (double **)calloc(matrizes[i].linhas,sizeof(double *));
         if(matrizes[i].matriz == NULL){
             desalocar_matriz(&matrizes, 1, tamanho);
@@ -92,6 +101,25 @@ Matrizes *alocar_matriz(int operacao, int tamanho){
     }
     return matrizes;
 }
+
+Matrizes *alocar_matriz_multiplicacao(int *quanti_matrizes){
+    int i,j,k;
+    j = quanti_matrizes;
+    for (i = 0; i < j; i++)
+    {
+        if (j%2 == 1)
+        {
+            quanti_matrizes++;
+            break;
+        }
+        
+    }
+    
+    Matrizes *matrizes = (Matrizes*)calloc(quanti_matrizes, sizeof(Matrizes));
+    
+    return matrizes;
+}
+
 
 void desalocar_matriz(Matrizes **matrizes, int desalocar, int quanti_matrizes){
     int i,j;
@@ -197,7 +225,7 @@ void multiplicacao(Matrizes *matrizes, int quanti_matrizes){
 
     for(i=0; i < matrizes[0].linhas; i++){
         for(j=0; j < matrizes[0].colunas; j++){
-            matrizes[0].matriz[i][j] = ;
+           
         }
     }
 }
